5b7b1440c3a2ffc641de3191a595270628880c6f
WujecDamian <wujecdamian12@gmail.com>
2026-02-13 00:13:50 +0100

    feat(): Make numbers work with fixed points

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 let displayText = document.querySelector('#display')
+let displayTextSmall = document.querySelector('#display__small')
+
 let clearBtn = document.querySelector('#clear')
 
 let num1 = 0
@@ -6,6 +8,7 @@ let num2 = 0
 let operator = ''
 clearBtn.addEventListener('click', () => {
   clearBtn.innerText = 'AC'
+  displayTextSmall.innerText = ''
 
   displayText.innerText = ''
   num1 = 0
@@ -15,56 +18,88 @@ clearBtn.addEventListener('click', () => {
 function add (a, b) {
   console.log(a + b)
   num1 = a + b
-  return a + b
+  if ((a + b).toFixed(3) <= 99999999) {
+    if (num1.toString().includes('.') || num2.toString().includes('.')) {
+      return (a + b).toFixed(3)
+    } else {
+      return a + b
+    }
+  } else {
+    return (a + b).toPrecision(4)
+  }
 }
 function substract (a, b) {
   console.log(a - b)
   num1 = a - b
-  return a - b
+  if ((a - b).toFixed(3) <= 99999999) {
+    return (a - b).toFixed(3)
+  } else {
+    return (a - b).toPrecision(4)
+  }
 }
 function multiply (a, b) {
   console.log(a * b)
   num1 = a * b
-  return a * b
+  if ((a * b).toFixed(3) <= 99999999) {
+    return (a * b).toFixed(3)
+  } else {
+    return (a * b).toPrecision(4)
+  }
 }
 function divide (a, b) {
   if (b > 0) {
     console.log(a / b)
     num1 = a / b
-    return a / b
+    if ((a / b).toFixed(3) <= 99999999) {
+      return (a / b).toFixed(3)
+    } else {
+      return (a / b).toPrecision(4)
+    }
   } else {
     console.log('Divide by 0? Really?')
     return 'Really?🤦‍♂️'
   }
 }
 
-let equals = document.querySelector('.equals')
-equals.addEventListener('click', () => {
+function operate () {
   if (num1 != 0 && num2 != 0 && operator != '') {
     if (operator == '÷') {
+      displayTextSmall.innerText = `${num1} ${operator} ${num2} =`
       displayText.innerText = divide(num1, num2)
     } else if (operator === '✕') {
+      displayTextSmall.innerText = `${num1} ${operator} ${num2} =`
       displayText.innerText = multiply(num1, num2)
     } else if (operator === '+') {
+      displayTextSmall.innerText = `${num1} ${operator} ${num2} =`
       displayText.innerText = add(num1, num2)
     } else if (operator === '-') {
+      displayTextSmall.innerText = `${num1} ${operator} ${num2} =`
       displayText.innerText = substract(num1, num2)
     }
   }
-})
+}
+let equals = document.querySelector('.equals')
+equals.addEventListener('click', operate)
 
 /* number variable  */
 let numbers = document.querySelectorAll('.number')
 numbers.forEach(element => {
   element.addEventListener('click', () => {
+    if (displayText.innerText.includes('.')) {
+      numbers[10].innerText = ''
+    } else {
+      numbers[10].innerText = '.'
+    }
     if (operator === '') {
       displayText.innerText += element.innerText
-      num1 = parseInt(displayText.innerText)
+      num1 = parseFloat(displayText.innerText)
     } else {
       if (num2 === 0) {
         displayText.innerText += element.innerText
-        num2 = parseInt(displayText.innerText)
+        num2 = parseFloat(displayText.innerText)
       } else {
+        displayText.innerText += element.innerText
+        num2 = parseFloat(displayText.innerText)
       }
     }
     console.log(num1, num2, operator)
@@ -78,9 +113,17 @@ operators.forEach(element => {
       if (operator === '') {
         displayText.innerText = element.innerText
         operator = element.innerText
+        displayTextSmall.innerText = `${num1} ${operator}`
+
         console.log(num1, num2, operator)
       }
       clearBtn.innerText = 'C'
+    }
+    if (num2 != 0) {
+      operate()
+      displayText.innerText = element.innerText
+      operator = element.innerText
+      displayTextSmall.innerText = `${num1} ${operator}`
     }
     displayText.innerText = ''
   })
