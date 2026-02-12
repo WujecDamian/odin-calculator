9dac8b617dc47a88044dfbc979c10913bc455d6c
WujecDamian <wujecdamian12@gmail.com>
2026-02-12 21:47:30 +0100

    feat: Add basic functions (add etc.)

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+let displayText = document.querySelector('#display')
+let clearBtn = document.querySelector('#clear')
+clearBtn.addEventListener(() => {
+  displayText.innerText = 'dsa'
+  a = 0
+  b = 0
+  operator = 0
+})
+function add (a, b) {
+  return a + b
+}
+function substract (a, b) {
+  return a - b
+}
+function multiply (a, b) {
+  return a * b
+}
+function divide (a, b) {
+  return a / b
+}
+
+function operate (a, b, operator) {}
