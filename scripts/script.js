348ab0804531b75684fe7b86d3fbe4c7fd95f5ce
WujecDamian <wujecdamian12@gmail.com>
2026-02-12 21:53:57 +0100

    feat: declare numbers and operator

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
 let displayText = document.querySelector('#display')
 let clearBtn = document.querySelector('#clear')
-clearBtn.addEventListener(() => {
-  displayText.innerText = 'dsa'
+
+let num1 = 0
+let num2 = 0
+let operator = ''
+clearBtn.addEventListener('click', () => {
+  displayText.innerText = '0'
   a = 0
   b = 0
   operator = 0
