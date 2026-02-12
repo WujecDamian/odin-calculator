c3a0369281630dec531b0e1b193625be12d163d8
WujecDamian <wujecdamian12@gmail.com>
2026-02-12 22:23:02 +0100

    feat: Add get numbers and operator functions

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,13 @@ let num1 = 0
 let num2 = 0
 let operator = ''
 clearBtn.addEventListener('click', () => {
-  displayText.innerText = '0'
+  displayText.innerText = ''
   a = 0
   b = 0
   operator = 0
 })
 function add (a, b) {
+  console.log(a + b)
   return a + b
 }
 function substract (a, b) {
@@ -24,3 +25,28 @@ function divide (a, b) {
 }
 
 function operate (a, b, operator) {}
+
+/* number variable  */
+let numbers = document.querySelectorAll('.number')
+numbers.forEach(element => {
+  element.addEventListener('click', () => {
+    if (operator === '') {
+      displayText.innerText += element.innerText
+      num1 = parseInt(displayText.innerText)
+    } else {
+      displayText.innerText += element.innerText
+
+      num2 = parseInt(displayText.innerText)
+    }
+  })
+})
+/* get operator */
+let operators = document.querySelectorAll('.operator')
+operators.forEach(element => {
+  element.addEventListener('click', () => {
+    if (operator === '') {
+      displayText.innerText += element.innerText
+      operator = element.innerText
+    }
+  })
+})
